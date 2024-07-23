e2a7c7606faa814ccde1ba17978bd62a1a243ca7
m1tk <mohammed.salih2@usmba.ac.ma>
2024-07-23 23:32:46 +0100

    TODO

--- transform.pl
+++ transform.pl
@@ -55,3 +55,42 @@ foreach my $key (keys %{$hash{"Decrypt"}}) {
 	$hash{"Encrypt"}{$key}{"100MB"}{time}."\\ (".$hash{"Encrypt"}{$key}{"100MB"}{th}.")], [".
 	$hash{"Encrypt"}{$key}{"1GB"}{time}."\\ (".$hash{"Encrypt"}{$key}{"1GB"}{th}.")],\n";
 }
+
+# generate plot data
+
+my %units = {
+    "B" => 1024*1024,
+    "KB" => 1024,
+    "MB" => 1,
+    "GB" => 1024
+};
+
+sub convert_to_mb {
+    my ($value) = @_;
+    
+    my ($type, $size);
+    if ($value =~ /^([-+]?\d*\.?\d+)\s*(\w+)\/$/) {
+        $size = $1;
+        $type = uc($2);
+    }
+    print "$value $type\n";
+
+    if (!exists $units{$type}) {
+        print "Invalid unit: $type\n";
+        exit 1;
+    }
+
+    if ($type eq "GB") {
+        return $size * $units{$type};
+    } else {
+        return $size / $units{$type};
+    }
+}
+
+foreach my $key (keys %{$hash{"Encrypt"}}) {
+	print convert_to_mb($hash{"Encrypt"}{$key}{"100B"}{th})."\n";
+	print convert_to_mb($hash{"Encrypt"}{$key}{"10KB"}{th})."\n";
+	print convert_to_mb($hash{"Encrypt"}{$key}{"1MB"}{th})."\n";
+	print convert_to_mb($hash{"Encrypt"}{$key}{"100MB"}{th})."\n";
+	print convert_to_mb($hash{"Encrypt"}{$key}{"1GB"}{th})."\n";
+}
